0886512821dfaaea0a1d54976e35bfec51f56223
LTeixeira4909 <119272952+LTeixeira4909@users.noreply.github.com>
2023-10-02 20:02:56 -0400

    See Mihir Field orientation is easy

--- src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -5,7 +5,7 @@ import com.pathplanner.lib.PathPlannerTrajectory;
 import com.pathplanner.lib.commands.PPSwerveControllerCommand;
 
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.filter.SlewRateLimiter; 
+import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
@@ -54,11 +54,10 @@ public class DrivetrainSubsystem extends SubsystemBase {
 
     Joystick js0 = new Joystick(0);
 
-
     private final double FRONT_LEFT_ENC_OFFSET = 67.5;
     private final double FRONT_RIGHT_ENC_OFFSET = 76.9;
     private final double BACK_RIGHT_ENC_OFFSET = -39.9;
-    private final double BACK_LEFT_ENC_OFFSET = 248.2;   
+    private final double BACK_LEFT_ENC_OFFSET = 248.2;
 
     Module leftModule = new Module("FrontLeft", 7, 8, 14, FRONT_LEFT_ENC_OFFSET);
     Module rightModule = new Module("FrontRight", 2, 1, 11, FRONT_RIGHT_ENC_OFFSET);
@@ -71,8 +70,8 @@ public class DrivetrainSubsystem extends SubsystemBase {
     SlewRateLimiter leftRightRateLimiter = new SlewRateLimiter(0.5);
     SlewRateLimiter turnRateLimiter = new SlewRateLimiter(0.5);
 
-    Pigeon2 pigeon = new Pigeon2(20);
-    //Global variable for drive rate speed
+    Pigeon2 pigeon = new Pigeon2(20, "CANivore1");
+    // Global variable for drive rate speed
     double m_driveRate = 1;
 
     public Rotation2d getGyroHeading() {
@@ -100,23 +99,25 @@ public class DrivetrainSubsystem extends SubsystemBase {
         }
         return m_inst;
     }
-    private SwerveModulePosition[] getModulePositions () {
+
+    private SwerveModulePosition[] getModulePositions() {
         SwerveModulePosition[] positions = new SwerveModulePosition[4];
-        positions[0] = leftModule.getPosition();  
-        positions[1] = rightModule.getPosition();  
+        positions[0] = leftModule.getPosition();
+        positions[1] = rightModule.getPosition();
         positions[2] = backRightModule.getPosition();
-        positions[3] = backLeftModule.getPosition();  
-       return positions;
-        
+        positions[3] = backLeftModule.getPosition();
+        return positions;
+
     }
+
     private DrivetrainSubsystem() {
         pigeon.setYaw(0);
 
         m_odometry = new SwerveDriveOdometry(
                 m_kinematics,
                 getGyroHeading(),
-             getModulePositions());
-        
+                getModulePositions());
+
         SmartDashboard.putBoolean("Done", false);
     }
 
@@ -143,12 +144,12 @@ public class DrivetrainSubsystem extends SubsystemBase {
         rightModule.periodic();
         backLeftModule.periodic();
         backRightModule.periodic();
-       
+
         SmartDashboard.putNumber("Pidgeon yaw", pigeon.getYaw());
         field.setRobotPose(m_odometry.getPoseMeters());
         SmartDashboard.putData(field);
         // Update the pose
-      
+
         SmartDashboard.putString("odo", m_odometry.getPoseMeters().toString());
         SmartDashboard.putString("odo", m_odometry.getPoseMeters().toString());
         // SmartDashboard.putNumber("Pose roation", m_pose.getRotation().getDegrees()) ;
@@ -199,15 +200,18 @@ public class DrivetrainSubsystem extends SubsystemBase {
         // Example chassis speeds: 1 meter per second forward, 3 meters
         // per second to the left, and rotation at 1.5 radians per second
         // counteclockwise.
-        //ChassisSpeeds speeds = new ChassisSpeeds(fwdBackDir, leftRightDir, turn);
-
-        
+        // ChassisSpeeds speeds = new ChassisSpeeds(fwdBackDir, leftRightDir, turn);
 
         ChassisSpeeds speeds = ChassisSpeeds.fromFieldRelativeSpeeds(
                 fwdBackDir * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND,
                 leftRightDir * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND,
                 turn * DrivetrainSubsystem.MAX_OMEGA_RADIANS_PER_SECOND,
                 Rotation2d.fromDegrees(pigeon.getYaw()));
+
+        // ChassisSpeeds speeds = new ChassisSpeeds(
+        // fwdBackDir * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND,
+        // leftRightDir * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND,
+        // turn * DrivetrainSubsystem.MAX_OMEGA_RADIANS_PER_SECOND);
 
         // Convert to module states
         SwerveModuleState[] moduleStates = m_kinematics.toSwerveModuleStates(speeds);
@@ -218,10 +222,10 @@ public class DrivetrainSubsystem extends SubsystemBase {
 
     public void DriveWithVelocity(double fwdBackDir, double leftRightDir, double turn) {
         ChassisSpeeds speeds = ChassisSpeeds.fromFieldRelativeSpeeds(
-            fwdBackDir * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND,
-            leftRightDir * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND,
-            turn * DrivetrainSubsystem.MAX_OMEGA_RADIANS_PER_SECOND,
-            Rotation2d.fromDegrees(pigeon.getYaw()));
+                fwdBackDir * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND,
+                leftRightDir * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND,
+                turn * DrivetrainSubsystem.MAX_OMEGA_RADIANS_PER_SECOND,
+                Rotation2d.fromDegrees(pigeon.getYaw()));
 
         // Convert to module states
         SwerveModuleState[] moduleStates = m_kinematics.toSwerveModuleStates(speeds);
@@ -254,8 +258,9 @@ public class DrivetrainSubsystem extends SubsystemBase {
         return new SequentialCommandGroup(
                 new InstantCommand(() -> {
                     if (isFirstPath)
-                        this.m_odometry.resetPosition(getGyroHeading(),getModulePositions(),traj.getInitialHolonomicPose());
-            
+                        this.m_odometry.resetPosition(getGyroHeading(), getModulePositions(),
+                                traj.getInitialHolonomicPose());
+
                 }),
                 new PPSwerveControllerCommand(
                         traj,
@@ -268,20 +273,21 @@ public class DrivetrainSubsystem extends SubsystemBase {
                         this),
                 new InstantCommand(() -> {
                     SmartDashboard.putBoolean("Done", true);
-                })).andThen(new InstantCommand(() -> {setModuleStates(m_kinematics.toSwerveModuleStates(new ChassisSpeeds(0,0,0)));}));
+                })).andThen(new InstantCommand(() -> {
+                    setModuleStates(m_kinematics.toSwerveModuleStates(new ChassisSpeeds(0, 0, 0)));
+                }));
     }
 
     public void setDriveRate(double driveRate) {
         m_driveRate = driveRate;
     }
+
     public double getDriveRate() {
         return m_driveRate;
     }
-    
 
     public void zeroGyro() {
         pigeon.setYaw(0d);
     }
-    
 
 }
